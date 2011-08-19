12451058d61951af2eaf5573aac49eb542c7d2bd
bugcloud <bugcloudisco@bugcloud.com>
2011-08-19 09:21:26 +0900

    fix bug at get random value

--- src/com/bugcloud/android/vg/share/Common.java
+++ src/com/bugcloud/android/vg/share/Common.java
@@ -2,7 +2,7 @@ package com.bugcloud.android.vg.share;
 
 public class Common {
 
-	public static int getRandom(int max, int min) {
+	public static int getRandom(int min, int max) {
 		int ret = (int)Math.floor(Math.random()*(max-min+1)) + min;
 		return ret;
 	}
